1cd5dbb3333a81e40cab60cad03ed3c0c6ae422d
WHL99 <xjv298347@gmail.com>
2022-08-21 17:49:20 +0200

    style and create RWD HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/HomePage.js";
 import NavBar from "./components/NavBar";
 
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,17 +2,17 @@ import { Nav, Navbar, Container } from 'react-bootstrap'
 
 function NavBar() {
   return (
-    <>
+    <div className='navbar'>
       <Navbar variant="dark"
-        sticky="top" expand="md" collapseOnSelect className="p-3" style={{backgroundColor:'gray', fontFamily:'Secular One'}}>
+        sticky="top" expand="md" collapseOnSelect className="p-1">
         <Container>
 
-          <Navbar.Brand href="#home">Estela Gracia</Navbar.Brand>
+          <Navbar.Brand href="/" style={{fontFamily:'Kaushan Script',fontSize:'1.7rem'}}>Estela Gracia</Navbar.Brand>
 
           <Navbar.Toggle />
           <Navbar.Collapse>
             <Nav>
-              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="#aboutMe">About Me</Nav.Link>
               <Nav.Link href="#gallery">Gallery</Nav.Link>
               <Nav.Link href="#multiMedia">Multi-Media</Nav.Link>
@@ -22,7 +22,7 @@ function NavBar() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </>
+    </div>
   );
 }
 
