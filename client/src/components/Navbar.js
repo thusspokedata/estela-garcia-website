df83f0d03018e632ad09fa82b20e785de3cfd819
WHL99 <xjv298347@gmail.com>
2022-08-21 15:02:46 +0200

    style and create RWD NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,25 +1,29 @@
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap'
 
 function NavBar() {
-    return (
-      <>
-        <Navbar bg="primary" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Estela Gracia</Navbar.Brand>
-            <Nav className="me-auto">
+  return (
+    <>
+      <Navbar variant="dark"
+        sticky="top" expand="md" collapseOnSelect className="p-3" style={{backgroundColor:'gray', fontFamily:'Secular One'}}>
+        <Container>
+
+          <Navbar.Brand href="#home">Estela Gracia</Navbar.Brand>
+
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#aboutMe">About Me</Nav.Link>
               <Nav.Link href="#gallery">Gallery</Nav.Link>
               <Nav.Link href="#multiMedia">Multi-Media</Nav.Link>
               <Nav.Link href="#event">Event</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
-
-
             </Nav>
-          </Container>
-        </Navbar>
-      </>
-    );
-  }
-  
-  export default NavBar;
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
